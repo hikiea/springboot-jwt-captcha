b34ed076b5b6b0d9303aa45fea1039c237d9dd7d
“y李忠毅 <lizhongyi@sinobest.cn>
2020-09-07 15:24:37 +0800

    fix: 修改readme文件;添加sql文件

--- src/main/java/com/lzy/springbootjwtcaptcha/annotation/PassToken.java
+++ src/main/java/com/lzy/springbootjwtcaptcha/annotation/PassToken.java
@@ -12,5 +12,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PassToken {
+
     boolean required() default true;
+
 }
